cdf2e712fd139085531d056555c47850ee9ac871
Miek Gieben <miek@miek.nl>
2014-05-15 20:41:54 +0100

    Remove tls stuff

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ import (
 
 var (
 	machines = strings.Split(os.Getenv("ETCD_MACHINES"), ",") // List of URLs to etcd
-	tlskey   = os.Getenv("ETCD_TLSKEY")                       // TLS private key path
-	tlspem   = os.Getenv("ETCD_TLSPEM")                       // X509 certificate
 	dnsaddr  = os.Getenv("DNS_ADDR")                          // Listen on this address
 )
 
@@ -23,14 +21,7 @@ func NewClient() (client *etcd.Client) {
 	if len(machines) == 1 && machines[0] == "" {
 		machines[0] = "http://127.0.0.1:4001"
 	}
-	if strings.HasPrefix(machines[0], "https://") {
-		var err error
-		if client, err = etcd.NewTLSClient(machines, tlspem, tlskey, ""); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		client = etcd.NewClient(machines)
-	}
+	client = etcd.NewClient(machines)
 	client.SyncCluster()
 	return client
 }
